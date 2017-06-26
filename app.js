99f9ebd25a6d572d80f586ff84ea6051b086c642
jason <jy1964@nyu.edu>
2017-06-26 13:57:21 -0400

    testing4

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function tests() {
-	console.log('awesome ------jekins---success---aaaaaaa-bbbbbb--ccc---ddd---e---fffff---ggggg');
+	console.log('awesome ------jekins---success---aaaaaaa-bbbbbb--ccc---ddd---e---f');
 }
