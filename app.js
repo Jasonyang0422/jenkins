c34c3cdd6b2e834d6f6fa17c0318b21fc66a2a0d
jason <jy1964@nyu.edu>
2017-06-26 13:16:22 -0400

    testing2

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function tests() {
-	console.log('awesome ------jekins---success---aaaaaaa-bbbbbb--ccc---ddd---e---fffff');
+	console.log('awesome ------jekins---success---aaaaaaa-bbbbbb--ccc---ddd---e---fffff---g');
 }
