eb15423db7a931a33c7021fd4924b35ee355bcca
jason <jy1964@nyu.edu>
2017-07-10 13:13:59 -0400

    add remote server

--- webdriver/driver.js
+++ webdriver/driver.js
@@ -5,7 +5,7 @@ function Driver(browser, mode) {
 	if(mode === 'remote') {
 		var driver = new webdriver.Builder()
 			.forBrowser(browser)
-			.usingServer('http://127.0.0.1:4444/wd/hub')
+			.usingServer('http://32764f9e.ngrok.io/wd/hub')
 		    // .withCapabilities({
 		    // 	'browserName': browser,
 		    // 	'platform': 'MAC',
