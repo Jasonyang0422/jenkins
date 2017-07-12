01ff50f9ab8f8b1a44df51489d923f262ad227ec
jason <jy1964@nyu.edu>
2017-07-12 14:09:30 -0400

    add FileSystem and screenshot

---
+++ utility/fileSystem.js
@@ -0,0 +1,71 @@
+var fs = require('fs');
+
+function FileSystem(filePath) {
+
+	this.filePath = filePath;
+
+	this.writeFile = writeFile;
+	this.readFile = readFile;
+	this.pushToArrayInFile = pushToArrayInFile;
+
+}
+
+module.exports = FileSystem;
+
+function writeFile(string) {
+	var that = this;
+
+	return Promise(function(resolve, reject) {
+		fs.writeFile(that.filePath, string, function(err) {
+		    if(err) { 
+		    	reject(err); 
+		    } else {
+				resolve("The file was saved!");
+		    }
+		});
+	});
+}
+
+function readFile() {
+	var that = this;
+
+	return Promise(function(resolve, reject) {
+		fs.readFile(that.filePath, "utf8", function(err, data) {
+			if(err) {
+				reject(err);
+			} else {
+				resolve(data);
+			}
+		});
+	});
+}
+
+function writeArryToFile(string, variableName) {
+	var that = this;
+
+	return this.writeFile("var " + variableName + " = ['" + string + "'];");
+}
+
+function pushToArrayInFile(string) {
+	var that = this;
+	
+	return this.readFile()
+		.then(function(originalStr) {
+			if(originalStr.length > 0) {
+				var len = originalStr.length;
+				var newStr = originalStr.slice(0, len - 1) + ", '" + string + "']";
+				return that.writeFile(newStr);
+			}
+		});
+}
+
+function imageStringProcessor(imageStr) {
+
+}
+
+
+
+
+
+
+
