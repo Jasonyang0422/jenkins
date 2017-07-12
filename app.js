216b6ebdeca35acebff1b95c52ea4d1dd40a46c0
jason <jy1964@nyu.edu>
2017-07-12 11:07:07 -0400

    create html report

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function tests() {
-	console.log('awesome ------jekins---success---aaaaaaa-bbbbbb--ccc---ddd---e---f-f-g-h-h-ii-kkk-kk-kk-k-kk-kk-ll-oo-aws-success-aaaaa');
+	console.log('awesome ------jekins---success---aaaaaaa-bbbbbb--ccc---ddd---e---f-f-g-h-h-ii-kkk-kk-kk-k-kk-kk-ll-oo-aws-success-aaaaaa');
 }
