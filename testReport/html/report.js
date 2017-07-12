45d96b5c87503f50612251effe53b63562da8a5e
jason <jy1964@nyu.edu>
2017-07-12 11:10:40 -0400

    create html report

--- testReport/html/report.js
+++ testReport/html/report.js
@@ -2,5 +2,5 @@ $( document ).ready(function() {
     console.log( "ready!" );
     var str = SCREENSHOTS[0];
     $("img").attr("src", "data:image/jpeg;base64, " + str);
-
+    setInterval(function(){ console.log("Hello"); }, 1000);
 });
